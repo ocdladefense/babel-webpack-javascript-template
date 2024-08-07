69eaa18456b86f25855b081f24ced0fb4f5efcb5
José Bernal <jbernal.web.dev@gmail.com>
2024-08-06 20:37:54 -0700

    Use mocking data for fetching Books Online content.

--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,22 @@ import App from '../components/App.jsx';
 
 // Data
 /* eslint-enable */
-import Chapter from '../data/ors-viewer/chapters/chapter-1.jsx';
 import Url from '@ocdla/lib-http/Url.js';
 import HttpClient from '@ocdla/lib-http/HttpClient.js';
 import OrsChapter from '@ocdla/ors/src/OrsChapter.js';
 import breadcrumbs from '../data/ors-viewer/breadcrumbs/items.json';
 import sidebarFirstItems from '../data/ors-viewer/sidebar_left/items.json';
 import sidebarSecondItems from '../data/ors-viewer/sidebar_right/items.json';
+
+// Mock data
+import Chapter from '../data/ors-viewer/chapters/chapter-1.jsx';
+import BonMock from './BonMock.js';
+
+if (USE_MOCK_BON) {
+    HttpClient.register('https://pubs.ocdla.org', new BonMock());
+}
+
+window.HttpClient = HttpClient;
 
 // An HTTP fragment, also known as a URI fragment, is a string of characters in a URL that identifies a specific location within a resource. The fragment identifier introduced by a hash mark (#) is the optional last part of a URL for a document. It is typically used to identify a portion of that document. The generic syntax is specified in RFC 3986. The hash mark separator in URIs does not belong to the fragment identifier.
 let url = new Url('https://appdev.ocdla.org/books-online/index.php');
