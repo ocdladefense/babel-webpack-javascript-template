71dcd621cdbf857fb9806bfb538472704c198d72
José Bernal <jbernal.web.dev@gmail.com>
2024-09-09 12:06:42 -0700

    Add new example App.jsx component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 /** @jsx vNode */ /** @jsxFrag "Fragment" */
-import { vNode, View } from '@ocdla/view/view.js';
+import { vNode, View } from "@ocdla/view/view.js";
 
-import Navbar from '@ocdla/global-components/src/components/Navbar';
-import Breadcrumbs from '@ocdla/global-components/src/components/Breadcrumbs';
-import Sidebar from '@ocdla/global-components/src/components/Sidebar';
-// import Body from '@ocdla/global-components/src/components/Body';
-import Footer from '@ocdla/global-components/src/components/Footer';
+import Navbar from "@ocdla/global-components/src/Navbar";
+import Breadcrumbs from "@ocdla/global-components/src/Breadcrumbs";
+import Sidebar from "@ocdla/global-components/src/Sidebar";
+// import Body from '@ocdla/global-components/src/Body';
+import Footer from "@ocdla/global-components/src/Footer";
 
 export default function App({
     view,
@@ -16,30 +16,30 @@ export default function App({
 }) {
     return (
         <>
-            <div class='group absolute right-0 m-4 flex gap-2 lg:left-0 lg:m-2'>
+            <div class="group absolute right-0 m-4 flex gap-2 lg:left-0 lg:m-2">
                 <button
-                    class='select-none whitespace-pre font-bold'
+                    class="select-none whitespace-pre font-bold"
                     onclick={() => {
-                        view.render('foo');
+                        view.render("foo");
                     }}></button>
             </div>
-            <header class='flex flex-col lg:h-32'>
+            <header class="flex flex-col lg:h-32">
                 <Navbar />
                 <Breadcrumbs
                     items={crumbs}
-                    separator='/'
+                    separator="/"
                 />
             </header>
-            <div class='container mx-auto border-x'>
-                <div class='lg:grid lg:grid-cols-6'>
+            <div class="container mx-auto border-x">
+                <div class="lg:grid lg:grid-cols-6">
                     <Sidebar items={sidebarFirstItems} />
-                    <main class='flex w-full flex-col gap-4 p-4 lg:col-span-4 lg:col-start-2 lg:me-auto lg:border-x lg:p-8'>
+                    <main class="flex w-full flex-col gap-4 p-4 lg:col-span-4 lg:col-start-2 lg:me-auto lg:border-x lg:p-8">
                         {children}
                     </main>
                     <Sidebar items={sidebarSecondItems} />
                 </div>
             </div>
-            <footer class='container mx-auto flex flex-col gap-4 border border-b-0 p-4 pb-16 lg:flex-row lg:gap-0 lg:p-8 lg:pb-16'>
+            <footer class="container mx-auto flex flex-col gap-4 border border-b-0 p-4 pb-16 lg:flex-row lg:gap-0 lg:p-8 lg:pb-16">
                 <Footer />
             </footer>
         </>
