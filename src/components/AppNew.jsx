8bfe7607e42fec4b510bd7706c040dafda2f990a
José Bernal <jbernal.web.dev@gmail.com>
2024-09-09 14:31:30 -0700

    Reorganize files.

--- src/components/AppNew.jsx
+++ src/components/AppNew.jsx
@@ -1,78 +1,70 @@
 /** @jsx vNode */ /** @jsxFrag "Fragment" */
-import { vNode, View } from "@ocdla/view/view.js";
-
+import { vNode, View } from "@ocdla/view";
 import Navbar from "@ocdla/global-components/src/Navbar";
-import Breadcrumbs from "@ocdla/global-components/src/Breadcrumbs";
-import Sidebar from "@ocdla/global-components/src/Sidebar";
-// import Body from '@ocdla/global-components/src/Body';
 import Footer from "@ocdla/global-components/src/Footer";
 
+export default function App({ headerPinned = false }) {
+    // There is a component that can be used to render a nice 404 error.
+    // return <NotFound />;
 
-export default function App({ headerPinned, breadcrumbs }) {
-  // There is a component that can be used to render a nice 404 error.
-  // return <NotFound />;
+    return (
+        <>
+            <header
+                class={`${headerPinned ? "sticky top-0" : ""}container mx-auto flex w-full flex-col bg-white lg:h-32`}>
+                <Navbar />
+            </header>
 
-  return (
-    <>
-      <header class={`${headerPinned ? 'sticky top-0 ' : ''}container mx-auto flex w-full flex-col bg-white lg:h-32`}>
-        <Navbar />
-      </header>
-      <Banner image={webpackInlinedImage} />
-      // Main component that supports one *or two-column layout.
-      <main>
-        <section cols="1">Some initial content and images here...</section>
-        <section cols="2" width="25%,75%">
-          <sidebar>
-            <HomePageLeftMenu items={theItems} />
-          </sidebar>
-          <div class="container mx-auto border-x">
-            <form>
-              <div class="space-y-12">
-                <div class="border-b border-gray-900/10 pb-12">
-                  <h2 class="text-base font-semibold leading-7 text-gray-900">
-                    Upload a file
-                  </h2>
+            <main>
+                <section cols="1">
+                    Some initial content and images here...
+                </section>
+                <section
+                    cols="2"
+                    width="25%,75%">
+                    <div class="container mx-auto border-x">
+                        <form>
+                            <div class="space-y-12">
+                                <div class="border-b border-gray-900/10 pb-12">
+                                    <h2 class="text-base font-semibold leading-7 text-gray-900">
+                                        Upload a file
+                                    </h2>
 
-                  <p class="mt-1 text-sm leading-6 text-gray-600">
-                    This information will be displayed publicly so be careful
-                    what you share.
-                  </p>
+                                    <p class="mt-1 text-sm leading-6 text-gray-600">
+                                        This information will be displayed
+                                        publicly so be careful what you share.
+                                    </p>
 
-                  <input type="file" class="mt-6" />
-                </div>
-              </div>
+                                    <input
+                                        type="file"
+                                        class="mt-6"
+                                    />
+                                </div>
+                            </div>
 
-              <div class="mt-6 flex items-center justify-end gap-x-6">
-                <button
-                  type="button"
-                  class="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Save
-                </button>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
-      <footer class="bg-wb-black text-wb-white container mx-auto border border-b-0 p-4 pb-16 lg:p-8 lg:pb-32">
-        <Social
-          social="facebook,twitter,youtube,gmaps"
-          showFacebook={true}
-          showTwitter={true}
-          showYouTube={true}
-          useGoogleMapsIFrame={true}
-        />
-        <Legal />
-        <FooterLinks>
-          {get the links from somewhere}
-        </FooterLinks>
-      </footer>
-    </>
-  );
+                            <div class="mt-6 flex items-center justify-end gap-x-6">
+                                <button
+                                    type="button"
+                                    class="text-sm font-semibold leading-6 text-gray-900">
+                                    Cancel
+                                </button>
+                                <button
+                                    type="submit"
+                                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                    Save
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </main>
+            <footer class="bg-wb-black text-wb-white container mx-auto border border-b-0 p-4 pb-16 lg:p-8 lg:pb-32">
+                <Footer
+                    showFacebook={true}
+                    showTwitter={true}
+                    showYouTube={true}
+                    useGoogleMapsIFrame={true}
+                />
+            </footer>
+        </>
+    );
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,59 +1,23 @@
-/** @jsx vNode */
-import '../css/input.css';
-import { vNode, View } from '@ocdla/view/view.js';
-import App from '../components/App.jsx';
+/** @jsx vNode */ /** @jsxFrag "Fragment" */
+import { vNode, View } from "@ocdla/view";
+import "../css/input.css";
+import HttpClient from "@ocdla/lib-http/HttpClient.js";
+import SiteMock from "../mock/SiteMock.js";
+import { parseReferences } from "@ocdladefense/ors/src/Outline.js";
+import App from "../components/AppNew";
 
-// Data
-/* eslint-enable */
-import Url from '@ocdla/lib-http/Url.js';
-import HttpClient from '@ocdla/lib-http/HttpClient.js';
-import OrsChapter from '@ocdla/ors/src/OrsChapter.js';
-import breadcrumbs from '../data/ors-viewer/breadcrumbs/items.json';
-import sidebarFirstItems from '../data/ors-viewer/sidebar_left/items.json';
-import sidebarSecondItems from '../data/ors-viewer/sidebar_right/items.json';
-
-// Mock data
-import Chapter from '../data/ors-viewer/chapters/chapter-1.jsx';
-import BonMock from './BonMock.js';
-
-if (USE_MOCK_BON) {
-    HttpClient.register('https://pubs.ocdla.org', new BonMock());
+if (USE_MOCK) {
+    HttpClient.register("https://example.com/", new SiteMock());
 }
 
-window.HttpClient = HttpClient;
+/**
+let text = "138.5(3)(a),(4)(a)-(c)";
+Parser.parseReferences(text);
+ */
+let parsed = parseReferences("(3)(a),(4)(a)-(c)");
+console.log(parsed);
 
-// An HTTP fragment, also known as a URI fragment, is a string of characters in a URL that identifies a specific location within a resource. The fragment identifier introduced by a hash mark (#) is the optional last part of a URL for a document. It is typically used to identify a portion of that document. The generic syntax is specified in RFC 3986. The hash mark separator in URIs does not belong to the fragment identifier.
-let url = new Url('https://appdev.ocdla.org/books-online/index.php');
+const $root = document.getElementById("root");
+const root = View.createRoot($root);
 
-url.buildQuery('chapter', '1');
-
-let req = new Request(url.toString());
-let client = new HttpClient();
-let resp = await client.send(req);
-let msword = await OrsChapter.fromResponse(resp);
-
-msword.chapterNum = 1;
-
-let xml = OrsChapter.toStructuredChapter(msword);
-
-/*
-    Inspect the available properties for use in building section outline (left nav) and content.
-    section outline (left nav) is listed in sectionTitles property.
-    xml.doc contains the entire document.
-    xml.toString() will return the entire document as an HTML string for use with innerHTML.
-*/
-
-console.log(xml);
-console.log(xml.doc);
-
-console.log('Loaded index.js');
-
-let root = View.createRoot('#app');
-root.render(
-    <App
-        crumbs={breadcrumbs}
-        sidebarFirstItems={sidebarFirstItems}
-        sidebarSecondItems={sidebarSecondItems}>
-        <Chapter />
-    </App>
-);
+root.render(<App />);
