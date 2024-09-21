6eef77c9b1df13b03eb81a76fcc83a8f1dd0d471
José Bernal <jbernal.web.dev@gmail.com>
2024-09-21 10:20:47 -0700

    Don't require a module to be installed for the application to build and load.

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,9 @@ import "../css/input.css";
 // Dynamic import of package components:
 // import MODULE_PATH + "/src/index.js";
 
+if(MODULE_PATH)
 await import(MODULE_PATH + "/src/index.js");
+
+else
+await import ("./main.js");
 
